a91c98efba9be64f755093a63d586373fdeedc87
bexigag <goncasjbb@gmail.com>
2025-03-09 23:25:14 +0000

    Menu and Parse input almost finished

--- Menu/Menu.h
+++ Menu/Menu.h
@@ -12,44 +12,114 @@ class Menu{
 public:
     Menu(){}
     string getfilename();
-    static void run();
+    void run();
 
 private:
-    string mode;
-    int source;
-    int dest;
-    vector<int> avoidNodes;
-    vector<pair<int,int>> avoidSegments;
-    int includeNode;
+    string mode = " ";
+    int source = -1;
+    int dest = -1;
+    vector<int> avoidNodes; //can be empty
+    vector<pair<int,int>> avoidSegments; //can be empty
+    int includeNode = -1; //can be empty
+    int maxWalkTime = -1;
     Graph<Location> graph;
 };
 
 
 void Menu::run() {
-    cout << "Menu" << endl;
-    cout << "1 Parse Input Data" << endl;
-    cout << "2 Route Planning Functionalities (driving only)" << endl;
-    cout << "3 Environmentally-Friendly Route Planning (driving and walking)" << endl;
-    cout << "4 Exit" << endl;
 
     bool flag = true;
     while(flag) {
         int option;
+        cout << "Menu (input a number >9 to run)" << endl;
+        cout << "0 Parse Input Data" << endl;
+        cout << "1 Choose Mode" << endl;
+        cout << "2 Choose source" << endl;
+        cout << "3 Choose Destination" << endl;
+        cout << "4 Choose AvoidNodes" << endl;
+        cout << "5 Choose AvoidSegments" << endl;
+        cout << "6 Choose IncludeNode" << endl;
+        cout << "7 Choose MaxWalkTime" << endl;
+        cout << "8 Reset" << endl;
+        cout << "9 Exit" << endl;
+        cout << "Choose an option:" << endl;
         cin >> option;
-        switch(option) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                flag = false;
-            break;
-            default:
-                cout << "Seu burro, só podes escolher até 4" << endl;
+        if (option == 0) {
+            string filename;
+            cout << "Enter file name or 'Graph' to build graph using Distances.csv and Locations.csv: " << endl;
+            cin >> filename;
+            Parse parse;
+            if (filename == "Graph") {
+                parse.readLocations(graph);
+                parse.readDistances(graph);
+            }
+            else {
+                parse.readInput(filename, source, dest, maxWalkTime, includeNode, avoidNodes, avoidSegments);
+            }
         }
-
+        else if (option == 1) {
+            cout << "Enter mode: " << endl;
+            cout << "1 driving " << endl;
+            cout << "2 driving-walking " << endl;
+            int m;
+            cin >> m;
+            if (m == 1) mode = "driving";
+            else if (m == 2) mode = "driving-walking";
+        }
+        else if (option == 2) {
+            cout << "Enter source: " << endl;
+            cin >> source;
+        }
+        else if (option == 3) {
+            cout << "Enter destination: " << endl;
+            cin >> dest;
+        }
+        else if (option == 4) {
+            avoidNodes.clear();
+            cout << "Enter avoidNodes (one at a time, to stop insert -1): " << endl;
+            int a;
+            cin >> a;
+            while (a != -1) {
+                avoidNodes.push_back(a);
+                cin >> a;
+            }
+        }
+        else if (option == 5) {
+            avoidNodes.clear();
+            cout << "Enter avoidSegments (one at a time, and in pairs, to stop insert -1): " << endl;
+            int b;
+            int c;
+            cin >> b;
+            cin >> c;
+            while (b != -1 && c != -1) {
+                avoidSegments.push_back({b, c});
+                cout << "Added (" << b << "," << c << ")" << endl;
+                cin >> b;
+                cin >> c;
+            }
+        }
+        else if (option == 6) {
+            cout << "Enter IncludeNode: " << endl;
+            cin >> includeNode;
+        }
+        else if (option == 7) {
+            cout << "Enter MaxWalkTime: " << endl;
+            cin >> maxWalkTime;
+        }
+        else if (option == 8) {
+            source = -1;
+            dest = -1;
+            maxWalkTime = -1;
+            includeNode = -1;
+            avoidNodes.clear();
+            avoidSegments.clear();
+        }
+        else if (option == 9) {
+            flag = false;
+        }
+        else {
+            cout << "Seu burro, só podes escolher até 4" << endl;
+        }
     }
 }
 
--- parse/Parse.h
+++ parse/Parse.h
@@ -12,19 +12,18 @@
 #include "location.h"
 #include <limits>
 
-template <class T>
 class Parse{
   public:
     Parse(){}
-    void readLocations(Graph<T> &graph);
-    void readDistances(Graph<T> &graph);
-    void readInput(std::string filename);
+    void readLocations(Graph<Location> &graph);
+    void readDistances(Graph<Location> &graph);
+    void readInput(std::string filename, int & source, int & dest, int & maxWalkTime, int & includeNode, std::vector<int> &avoidNodes, std::vector<std::pair<int,int>> &avoidSegments);
 
 };
 
-template <class T>
-void Parse<T>::readLocations(Graph<T>& graph) {
-  std::ifstream file("../DataSet/Locations.csv");
+
+void Parse::readLocations(Graph<Location> & graph) {
+  std::ifstream file("C:\\Users\\gonca\\Desktop\\Desktopp\\DA\\ProjetoDA\\DataSet\\Locations.csv");
   if (!file.is_open()) {
     std::cerr << "Error opening file Locations.csv" << std::endl;
     return;
@@ -42,20 +41,23 @@ void Parse<T>::readLocations(Graph<T>& graph) {
     getline(ss, code, ',');
     getline(ss, parking, ',');
 
+
     int id = std::stoi(idstr);
     bool hasparking;
     if (parking == "1") hasparking = true;
     else hasparking = false;
 
-    graph.addVertex(new Location(location, id, code,hasparking));
+    graph.addVertex(Location(location, id, code,hasparking));
   }
+  file.close();
 }
 
-template <class T>
-void Parse<T>::readDistances(Graph<T>& graph) {
-  std::ifstream file("../DataSet/Distances.csv");
+
+void Parse::readDistances(Graph<Location> & graph) {
+  std::ifstream file("C:\\Users\\gonca\\Desktop\\Desktopp\\DA\\ProjetoDA\\DataSet\\Distances.csv");
   if (!file.is_open()) {
     std::cerr << "Error opening file Distances.csv" << std::endl;
+    return;
   }
 
   std::string line;
@@ -67,16 +69,68 @@ void Parse<T>::readDistances(Graph<T>& graph) {
 
     getline(ss, code1, ',');
     getline(ss, code2, ',');
-    getline(ss, walking, ',');
     getline(ss, driving, ',');
+    getline(ss, walking, ',');
+
 
     if (driving == "X"){
-      graph.addEdge(code1, code2, INF, stoi(walking));
+      graph.addEdge(Location(" ",-1,code1,false), Location(" ",-1,code2,false), INF, stoi(walking));
     }
-    else graph.addEdge(code1, code2, stoi(driving), stoi(walking));
+    else graph.addEdge(Location(" ",-1,code1,false), Location(" ",-1,code2,false), stoi(driving), stoi(walking));
   }
+  file.close();
 }
 
+void Parse::readInput(std::string filename, int & source, int & dest, int & maxWalkTime, int & includeNode, std::vector<int> &avoidNodes, std::vector<std::pair<int,int>> &avoidSegments) {
+  std::ifstream file("../DataSet/" + filename);
+  if (!file.is_open()) {
+    std::cerr << "Error opening file " << filename << std::endl;
+    return;
+  }
+  std::string line;
+
+  while (getline(file, line)) {
+    std::stringstream ss(line);
+    std::string key;
+    getline(ss, key, ':');
+
+    if (key == "Source") {
+      ss >> source;
+    } else if (key == "Destination") {
+      ss >> dest;
+    } else if (key == "MaxWalkTime") {
+      ss >> maxWalkTime;
+    } else if (key == "IncludeNode") {
+      ss >> includeNode;
+    } else if (key == "AvoidNodes") {
+      avoidNodes.clear();
+      std::string value;
+      while (getline(ss, value, ',')) {
+        if (!value.empty()) {
+          avoidNodes.push_back(stoi(value));
+        }
+      }
+    } else if (key == "AvoidSegments") {
+      avoidSegments.clear();
+      std::string segment;
+      while (getline(ss, segment, ')')) {
+        if (segment.empty() || segment == "(") continue;
+        size_t start = segment.find('(');
+        if (start != std::string::npos) {
+          segment = segment.substr(start + 1);  // Remove o '('
+        }
+        std::stringstream segmentStream(segment);
+        std::string loc1, loc2;
+        getline(segmentStream, loc1, ',');
+        getline(segmentStream, loc2, ',');
+        if (!loc1.empty() && !loc2.empty()) {
+          avoidSegments.emplace_back(stoi(loc1), stoi(loc2));
+        }
+      }
+    }
+  }
+  file.close();
+}
 
 
 #endif //PARSE_H
