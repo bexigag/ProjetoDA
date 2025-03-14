965bc43c5722e0585637f954158fb08fa238e8d6
diogo-ribeiro-05 <diogodinisribeiro@gmail.com>
2025-03-14 10:58:14 +0000

    add reset graph

--- Algorithms/Algorithm.h
+++ Algorithms/Algorithm.h
@@ -20,6 +20,7 @@ class Algorithm{
     private:
       void distra(Graph<Location> & graph, Vertex<Location> *src);
       void algorithm2_1(Graph<Location> & graph, const int& source, const int& dest);
+      void resetGraph(Graph<Location> & graph);
 };
 
 void Algorithm::runAlgorithm(Graph<Location> & graph,const std::string& mode,const int & source,const int & dest,const int & maxWalkTime,const int & includeNode,const std::vector<int> &avoidNodes,const std::vector<std::pair<int,int>> &avoidSegments){
@@ -43,44 +44,55 @@ void Algorithm::algorithm2_1(Graph<Location> & graph, const int& source, const i
 
   int distance = 0;
   std::stack<int> path;
-  path.push(dst->getInfo().getId());
-  distance += dst->getPath()->getWeightD();
-  dst = dst->getPath()->getOrig();
-  while (dst != src){
-    Edge<Location> *e = dst->getPath();
-    distance += e->getWeightD();
-    path.push(dst->getInfo().getId());
-    dst->setProcessing(true);
-    dst = e->getOrig();
-  }
-  path.push(src->getInfo().getId());
 
-  while (!path.empty()) {
-    std::cout << path.top();
-    path.pop();
-    if (!path.empty()){std::cout << ",";}
+  if (dst->getPath() == nullptr) {
+    std::cout << "none" << std::endl;
   }
-  std::cout<< "(" << distance << ")"<< std::endl;
+  else {
+    path.push(dst->getInfo().getId());
+    distance += dst->getPath()->getWeightD();
+    dst = dst->getPath()->getOrig();
+    while (dst != src){
+      Edge<Location> *e = dst->getPath();
+      distance += e->getWeightD();
+      path.push(dst->getInfo().getId());
+      dst->setProcessing(true);
+      dst = e->getOrig();
+    }
+    path.push(src->getInfo().getId());
+
+    while (!path.empty()) {
+      std::cout << path.top();
+      path.pop();
+      if (!path.empty()){std::cout << ",";}
+    }
+    std::cout<< "(" << distance << ")"<< std::endl;
+  }
 
   std::cout << "AlternativeDrivingRoute:";
   distra(graph,src);
-  distance = 0;
-  dst = dst1;
-  while (dst != src){
-    Edge<Location> *e = dst->getPath();
-    distance += e->getWeightD();
-    path.push(dst->getInfo().getId());
-    dst = e->getOrig();
+  if (dst->getPath() == nullptr) {
+    std::cout << "none" << std::endl;
   }
+  else {
+    distance = 0;
+    dst = dst1;
+    while (dst != src){
+      Edge<Location> *e = dst->getPath();
+      distance += e->getWeightD();
+      path.push(dst->getInfo().getId());
+      dst = e->getOrig();
+    }
 
-  path.push(src->getInfo().getId());
+    path.push(src->getInfo().getId());
 
-  while (!path.empty()) {
-    std::cout << path.top();
-    path.pop();
-    if (!path.empty()){std::cout << ",";}
+    while (!path.empty()) {
+      std::cout << path.top();
+      path.pop();
+      if (!path.empty()){std::cout << ",";}
+    }
+    std::cout<< "(" << distance << ")"<< std::endl;
   }
-  std::cout<< "(" << distance << ")"<< std::endl;
 }
 
 void Algorithm::distra(Graph<Location> & graph, Vertex<Location> *src){
@@ -113,6 +125,15 @@ void Algorithm::distra(Graph<Location> & graph, Vertex<Location> *src){
         }
       }
     }
+}
+
+void Algorithm::resetGraph(Graph<Location> & graph) {
+  for (Vertex<Location> *v :graph.getVertexSet()) {
+    v->setProcessing(false);
+    for (Edge<Location> *e : v->getAdj()) {
+      e->setSelected(false);
+    }
+  }
 }
 
 #endif //ALGORITHM_H
